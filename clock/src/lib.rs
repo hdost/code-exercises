7e1ac63b95fd7b93669095b2e0039791587db81c
Harold Dost <harolddost@gmail.com>
2021-09-25 14:02:16 +0200

    Finish the clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,56 +1,56 @@
-use std::fmt;
 use std::cmp;
+use std::fmt;
 
 #[derive(fmt::Debug)]
 pub struct Clock {
-   hours: i32,
-   minutes: i32,
+    hours: i32,
+    minutes: i32,
+}
+// If minutes are >= 60 add time to hours
+// If minutes are < 0 remove time from hours
+fn minutes_delta(minutes: i32) -> (i32, i32) {
+    let mut new_minutes = minutes % 60;
+    let mut adj_hours = minutes / 60;
+    if minutes < 0 {
+        adj_hours = minutes / 60;
+        if minutes.abs() % 60 > 0 {
+            adj_hours -= 1;
+        }
+        new_minutes = (60 + minutes % 60) % 60;
+    }
+    (adj_hours, new_minutes)
+}
+fn hours_delta(hours: i32, hours_delta: i32) -> i32 {
+    let new_hours: i32;
+    if hours + hours_delta < 0 {
+        new_hours = (24 - (hours + hours_delta).abs() % 24) % 24;
+    } else {
+        new_hours = (hours + hours_delta) % 24;
+    }
+    new_hours
 }
 
 impl Clock {
-
-    // If minutes are >= 60 add time to hours
-    // If minutes are < 0 remove time from hours
-    fn minutes_delta(minutes: i32) -> (i32, i32) {
-        let mut new_minutes = minutes % 60;
-        let mut adj_hours = minutes / 60;
-        if minutes < 0 {
-            adj_hours = minutes / 60;
-            if minutes.abs() % 60 > 0 {
-                adj_hours-=1;
-            }
-            new_minutes = (60 + minutes % 60 ) % 60;
-        }
-        (adj_hours, new_minutes)
-    }
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut new_hours = hours;
-        let mut new_minutes = minutes % 60;
-        let mut adj_hours = minutes / 60;
-        if minutes < 0 {
-            adj_hours = minutes / 60;
-            if minutes.abs() % 60 > 0 {
-                adj_hours-=1;
-            }
-            new_minutes = (60 + minutes % 60 ) % 60;
+        let (adj_hours, new_minutes) = minutes_delta(minutes);
+        Clock {
+            hours: hours_delta(hours, adj_hours),
+            minutes: new_minutes,
         }
-        if hours + adj_hours < 0 {
-            new_hours = 24 - (hours + adj_hours).abs() % 24;
-        }
-        else{
-            new_hours = (hours + adj_hours) % 24;
-        }
-        Clock{hours:(new_hours) % 24,minutes: new_minutes}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let (adj_hours, new_minutes) = minutes_delta(minutes + self.minutes);
+        Clock {
+            hours: hours_delta(self.hours, adj_hours),
+            minutes: new_minutes,
+        }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-       write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
 
