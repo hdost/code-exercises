a6f13e237cf9a3f39a6405c4b5a0bbd03751e8d0
Harold Dost <harolddost@gmail.com>
2021-12-08 09:41:45 +0100

    FInished day 7

--- 2021/day07/src/lib.rs
+++ 2021/day07/src/lib.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 #[test]
+#[ignore]
 fn test_first_part_sample_data(){
     let file = File::open("test.txt");
     match file {
@@ -34,7 +35,6 @@ fn test_second_part_sample_data(){
 }
 
 #[test]
-#[ignore]
 fn test_second_part_real_data(){
     let file = File::open("input.txt");
     match file {
@@ -44,26 +44,74 @@ fn test_second_part_real_data(){
 
 }
 
+fn net_difference(crabs:&Vec<i32>, pos:i32) -> i32 {
+    let mut fuel_spent = 0;
+    for c in crabs {
+        fuel_spent += (c - pos).abs()
+    }
+    fuel_spent
+}
+
+fn net_difference_triangle(crabs:&Vec<i32>, pos:i32) -> i32 {
+    let mut fuel_spent = 0;
+    for c in crabs {
+        let n = (c - pos).abs();
+        fuel_spent += (n * (n + 1) )/2;
+    }
+    fuel_spent
+}
+
 fn process_file(file: File) {
     let reader = BufReader::new(file);
     let lines = reader.lines();
-    let mut sums = Vec::new();
-    let mut max = 0;
-    for (i,line) in lines.enumerate() {
-        if let Ok(line) = line {
+    let mut crabs = Vec::new();
+    for line in lines.into_iter().flatten() {
+        println!("{}",line);
+        for pos in line.split(',').into_iter() {
+            let pos = pos.parse::<i32>().unwrap();
+            crabs.push(pos);
         }
     }
-    println!("Output: {}", gamma * epsilon);
+    crabs.sort();
+    println!("{:?}",crabs);
+    let bottom = *crabs.first().unwrap();
+    let top = *crabs.last().unwrap();
+    let (mut best_cost,mut best_pos) = (net_difference(&crabs,bottom),bottom);
+    for i in (bottom+1)..=top {
+        let cost = net_difference(&crabs,i);
+        if cost < best_cost {
+            best_cost = cost;
+            best_pos = i;
+        }
+    }
+
+    println!("Best Cost: {} Best Pos: {}", best_cost, best_pos);
 }
 
 
 fn process_file_2(file: File) {
     let reader = BufReader::new(file);
     let lines = reader.lines();
-    for (i,line) in lines.enumerate() {
-        if let Ok(line) = line {
+    let mut crabs = Vec::new();
+    for line in lines.into_iter().flatten() {
+        println!("{}",line);
+        for pos in line.split(',').into_iter() {
+            let pos = pos.parse::<i32>().unwrap();
+            crabs.push(pos);
+        }
+    }
+    crabs.sort();
+    println!("{:?}",crabs);
+    let bottom = *crabs.first().unwrap();
+    let top = *crabs.last().unwrap();
+    let (mut best_cost,mut best_pos) = (net_difference_triangle(&crabs,bottom),bottom);
+    for i in (bottom+1)..=top {
+        let cost = net_difference_triangle(&crabs,i);
+        if cost < best_cost {
+            best_cost = cost;
+            best_pos = i;
         }
     }
 
-    println!("Output: {}", o2 * co2);
+    println!("Best Cost: {} Best Pos: {}", best_cost, best_pos);
 }
