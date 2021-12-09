90f76270ff562c372d702f0f7ce9986ef18a288b
Harold Dost <harolddost@gmail.com>
2021-12-09 23:33:08 +0100

    Things before work

--- 2021/day09/src/lib.rs
+++ 2021/day09/src/lib.rs
@@ -3,8 +3,17 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+fn str_to_num_vec(input: &str) -> Vec<i32> {
+    let mut output = Vec::new();
+    for c in input.chars() {
+        output.push(c as i32 - 48);
+    }
+    output
+}
+
 #[test]
-fn test_first_part_sample_data(){
+#[ignore]
+fn test_first_part_sample_data() {
     let file = File::open("test.txt");
     match file {
         Err(e) => println!("Cannot find file {}", e),
@@ -13,19 +22,17 @@ fn test_first_part_sample_data(){
 }
 
 #[test]
-#[ignore]
-fn test_first_part_real_data(){
+fn test_first_part_real_data() {
     let file = File::open("input.txt");
     match file {
         Err(e) => println!("Cannot find file {}", e),
         Ok(file) => process_file(file),
     }
-
 }
 
 #[test]
 #[ignore]
-fn test_second_part_sample_data(){
+fn test_second_part_sample_data() {
     let file = File::open("test.txt");
     match file {
         Err(e) => println!("Cannot find file {}", e),
@@ -35,35 +42,81 @@ fn test_second_part_sample_data(){
 
 #[test]
 #[ignore]
-fn test_second_part_real_data(){
+fn test_second_part_real_data() {
     let file = File::open("input.txt");
     match file {
         Err(e) => println!("Cannot find file {}", e),
         Ok(file) => process_file_2(file),
     }
+}
 
+fn num_lower_than_point(num: i32, vec: &Vec<Vec<i32>>, x: i32, y: i32) -> bool {
+    match vec.get(x as usize) {
+        Some(l) => match l.get(y as usize) {
+            Some(n) => {
+                if num < *n {
+                    true
+                } else {
+                    false
+                }
+            }
+            None => true,
+        },
+        None => true,
+    }
+}
+
+fn is_low_point(vec: &Vec<Vec<i32>>, x: i32, y: i32) -> bool {
+    let curr = *vec.get(x as usize).unwrap().get(y as usize).unwrap();
+    let below = num_lower_than_point(curr, vec, x, y + 1);
+    let above = num_lower_than_point(curr, vec, x, y - 1);
+    let left = num_lower_than_point(curr, vec, x - 1, y);
+    let right = num_lower_than_point(curr, vec, x + 1, y);
+    below && above && left & right
+}
+
+fn find_low_points(vec: &Vec<Vec<i32>>) -> Vec<(i32, i32)> {
+    let mut points = Vec::new();
+    for (x, l) in vec.iter().enumerate() {
+        for (y, _) in l.iter().enumerate() {
+            if is_low_point(vec, x as i32, y as i32) {
+                points.push((x as i32, y as i32));
+            }
+        }
+    }
+
+    points
+}
+
+fn sum_points(matrix:&Vec<Vec<i32>>,points:&Vec<(i32,i32)>) -> i32 {
+    let mut sum = 0;
+
+    for p in points {
+        sum += matrix.get(p.0 as usize).unwrap().get(p.1 as usize).unwrap();
+    }
+    sum
 }
 
 fn process_file(file: File) {
     let reader = BufReader::new(file);
     let lines = reader.lines();
-    let mut sums = Vec::new();
-    let mut max = 0;
-    for (i,line) in lines.enumerate() {
-        if let Ok(line) = line {
-        }
+    let mut matrix = vec![];
+    for line in lines.flatten() {
+        matrix.push(str_to_num_vec(&line));
     }
-    println!("Output: {}", gamma * epsilon);
-}
+    let low_points = find_low_points(&matrix);
 
+    println!("Output: {}", sum_points(&matrix,&low_points) + low_points.len() as i32);
+}
 
 fn process_file_2(file: File) {
     let reader = BufReader::new(file);
     let lines = reader.lines();
-    for (i,line) in lines.enumerate() {
-        if let Ok(line) = line {
-        }
+    let mut matrix = vec![];
+    for line in lines.flatten() {
+        matrix.push(str_to_num_vec(&line));
     }
+    let low_points = find_low_points(&matrix);
 
-    println!("Output: {}", o2 * co2);
+    println!("Output: {}", sum_points(&matrix,&low_points) + low_points.len() as i32);
 }
