3b8e65aeab1f3201fb59a37cb6bd074ada438fac
Harold Dost <h.dost@criteo.com>
2023-12-01 07:22:02 +0100

    Initialize 2023 AOC

---
+++ advent-of-code/rust/2023/dayX/src/lib.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn process_file(file: File) -> i64 {
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    for line in lines.flatten() {}
+
+    1
+}
+
+fn process_file_2(file: File) -> i64 {
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    for line in lines.flatten() {}
+
+    1
+}
+
+#[test]
+fn test_first_part_sample_data() {
+    let file = File::open("test.txt");
+    match file {
+        Err(e) => println!("Cannot find file {}", e),
+        Ok(file) => {
+            assert_eq!(17, process_file(file));
+        }
+    }
+}
+
+#[test]
+#[ignore]
+fn test_first_part_real_data() {
+    let file = File::open("input.txt");
+    match file {
+        Err(e) => println!("Cannot find file {}", e),
+        Ok(file) => {
+            assert_eq!(1, process_file(file));
+        }
+    }
+}
+
+#[test]
+#[ignore]
+fn test_second_part_sample_data() {
+    let file = File::open("test.txt");
+    match file {
+        Err(e) => println!("Cannot find file {}", e),
+        Ok(file) => {
+            assert_eq!(1, process_file_2(file));
+        }
+    }
+}
+
+#[test]
+#[ignore]
+fn test_second_part_real_data() {
+    let file = File::open("input.txt");
+    match file {
+        Err(e) => println!("Cannot find file {}", e),
+        Ok(file) => {
+            assert_eq!(1, process_file_2(file));
+        }
+    }
+}
